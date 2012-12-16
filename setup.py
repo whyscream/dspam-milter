385486ba8d09fa2d922904ac827e7b2e90d6c073
Tom Hendrikx <tom@whyscream.net>
2012-12-16 19:24:03 +0100

    Added basic README and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2012, Tom Hendrikx
+# All rights reserved.
+#
+# See LICENSE for the license.
+
+from distutils.core import setup
+
+setup(name='pydspam',
+    description='Python utilities for interacting with DSPAM ',
+    author='Tom Hendrikx',
+    author_email='pydspam@whyscream.net',
+    url='https://github.com/whyscream/pydspam',
+    version='0.1',
+    packages=['dspam'],
+    scripts=['bin/dspam-milter'],
+    data_files=[('/etc/', ['bin/dspam-milter.cfg'])],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email :: Filters'
+    ],
+    license='New (3-clause) BSD License',
+)
